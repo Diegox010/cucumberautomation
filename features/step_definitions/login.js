05105ba85674fcb2487913d8d8f04a7ec1abad40
IsaacLRubio <isaac.lopez@enroutesystems.com>
2018-03-27 15:49:16 -0600

    setTimeout and gitignore added

--- features/step_definitions/login.js
+++ features/step_definitions/login.js
@@ -6,17 +6,23 @@ var {expect} = require('chai');
 
 defineSupportCode(function({Given, When, Then}) {
     Given(/^I want to sign in to my Outlook account$/, function() {
-		this.driver.get("https://outlook.live.com/owa/");
-		this.driver.findElement(By.css("body > section > div > div.header > div.headerHero > div > div")).click();
+      setTimeout( () => {
+        this.driver.get("https://outlook.live.com/owa/");
+        this.driver.findElement(By.css("body > section > div > div.header > div.headerHero > div > div")).click();
+      }, 2000);
     });
 
     When(/^I enter my email "([^"]*)"$/, function(email) {
-    	this.driver.findElement(By.id("i0116")).sendKeys(email);
-    	this.driver.findElement(By.id("idSIButton9")).submit();
+      setTimeout( () => {
+        this.driver.findElement(By.id("i0116")).sendKeys(email);
+        this.driver.findElement(By.id("idSIButton9")).submit();
+      }, 6000);
     });
 
     Then(/^my password "([^"]*)"$/, function(password) {
-    	this.driver.findElement(By.id("i0118")).sendKeys(password);
-    	this.driver.findElement(By.id("idSIButton9")).submit();
+      setTimeout( () => {
+        this.driver.findElement(By.id("i0118")).sendKeys(password);
+        this.driver.findElement(By.id("idSIButton9")).submit();
+      }, 10000);
     });
 });
