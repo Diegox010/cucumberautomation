80d5ef05336d364b97c7c3cf81d4c099e975ad75
DiegoEA <diego.espinosa@enroutesystems.com>
2018-03-28 10:37:25 -0600

    Changed to .bind() instead of using the fat arrow

--- features/step_definitions/login.js
+++ features/step_definitions/login.js
@@ -6,23 +6,23 @@ var {expect} = require('chai');
 
 defineSupportCode(function({Given, When, Then}) {
     Given(/^I want to sign in to my Outlook account$/, function() {
-      setTimeout( () => {
+      setTimeout(function() {
         this.driver.get("https://outlook.live.com/owa/");
         this.driver.findElement(By.css("body > section > div > div.header > div.headerHero > div > div")).click();
-      }, 2000);
+      }.bind(this), 2000);
     });
 
     When(/^I enter my email "([^"]*)"$/, function(email) {
-      setTimeout( () => {
+      setTimeout(function() {
         this.driver.findElement(By.id("i0116")).sendKeys(email);
         this.driver.findElement(By.id("idSIButton9")).submit();
-      }, 6000);
+      }.bind(this), 6000);
     });
 
     Then(/^my password "([^"]*)"$/, function(password) {
-      setTimeout( () => {
+      setTimeout(function() {
         this.driver.findElement(By.id("i0118")).sendKeys(password);
         this.driver.findElement(By.id("idSIButton9")).submit();
-      }, 10000);
+      }.bind(this), 10000);
     });
 });
